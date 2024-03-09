9068ffa00b21b9162b4ec8e50e55710f751db7b4
sevval14 <sevvalozekinci85@gmail.com>
2024-03-09 19:45:07 +0300

    added security sections for put delete and delete process

---
+++ src/main/java/com/example/fitbull/security/AppWebSecurityConfigurer.java
@@ -0,0 +1,23 @@
+package com.example.fitbull.security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AppWebSecurityConfigurer {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable() // CSRF korumasını devre dışı bırak
+            .authorizeRequests()
+                .requestMatchers("/users/**").permitAll() // '/public/' URL patikası altındaki tüm istekler için güvenlik gereksinimlerini devre dışı bırak
+                .anyRequest().authenticated() // Diğer tüm isteklerin kimlik doğrulamasını gerektir
+            .and()
+            .httpBasic(); // HTTP Basic kimlik doğrulama kullan
+        return http.build();
+    }
+}
