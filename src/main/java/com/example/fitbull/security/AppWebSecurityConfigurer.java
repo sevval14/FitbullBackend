45bd6c5eb7a850e574e287939eee46109b623b09
sevval14 <sevvalozekinci85@gmail.com>
2024-03-25 03:15:06 +0300

    added some securtiy par

--- src/main/java/com/example/fitbull/security/AppWebSecurityConfigurer.java
+++ src/main/java/com/example/fitbull/security/AppWebSecurityConfigurer.java
@@ -14,7 +14,8 @@ public class AppWebSecurityConfigurer {
         http
             .csrf().disable() // CSRF korumasını devre dışı bırak
             .authorizeRequests()
-                .requestMatchers("/users/**").permitAll() // '/public/' URL patikası altındaki tüm istekler için güvenlik gereksinimlerini devre dışı bırak
+                .requestMatchers("/users/**").permitAll()
+                .requestMatchers("/gym/**").permitAll()
                 .anyRequest().authenticated() // Diğer tüm isteklerin kimlik doğrulamasını gerektir
             .and()
             .httpBasic(); // HTTP Basic kimlik doğrulama kullan
