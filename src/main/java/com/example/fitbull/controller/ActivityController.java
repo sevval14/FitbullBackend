36c35b24852046b21a5b0148124c1700a07f8b09
sevval14 <sevvalozekinci85@gmail.com>
2024-03-27 00:57:35 +0300

    change some part but code doesnt work we will contiune on that

--- src/main/java/com/example/fitbull/controller/ActivityController.java
+++ src/main/java/com/example/fitbull/controller/ActivityController.java
@@ -1,45 +1,47 @@
 package com.example.fitbull.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.fitbull.entities.Activity;
 import com.example.fitbull.repo.ActivityRepository;
 import com.example.fitbull.repo.GymRepository;
 
 @RestController
-@RequestMapping("/gyms/activities")
+@RequestMapping("/activities")
 public class ActivityController {
-	
-	
-	
+
     @Autowired
-	GymRepository gymRepository;
-    
+    private ActivityRepository activityRepository;
+
     @Autowired
-    ActivityRepository activityRepository;
-    
-   
-	
-	public ActivityController(GymRepository gymRepository,ActivityRepository activityRepository) {
-		this.gymRepository=gymRepository;
-		this.activityRepository=activityRepository;
-	}
-	
-	@GetMapping
-	public List<Activity> getAllActivity() {
-		return activityRepository.findAll();
-	}
+    private GymRepository gymRepository;
 
-	
+    // Tüm aktiviteleri listele
+    @GetMapping
+    public List<Activity> getAllActivities() {
+        return activityRepository.findAll();
+    }
 
-	
-
-
-
-	
+    // Yeni bir aktivite oluştur
+    @PostMapping
+    public ResponseEntity<?> createActivity(@RequestBody Activity newActivity) {
+        if (newActivity.getGym() != null && newActivity.getGym().getId() != null) {
+            // Gym'in varlığını kontrol et
+            return gymRepository.findById(newActivity.getGym().getId())
+                .map(gym -> {
+                    newActivity.setGym(gym); // Gym'i ayarla
+                    Activity savedActivity = activityRepository.save(newActivity); // Aktiviteyi kaydet
+                    return ResponseEntity.ok(savedActivity); // Kaydedilen aktiviteyi dön
+                })
+                .orElse(ResponseEntity.notFound().build()); // Eğer Gym bulunamazsa, 404 dön
+        } else {
+            return ResponseEntity.badRequest().body("Gym ID is required");
+        }
+    }
 }
